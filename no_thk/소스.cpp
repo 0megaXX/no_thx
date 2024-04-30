ab50af2a73e0484d1607b97612492e9fd26ed564
0megaXX <270pp13@naver.com>
2024-04-30 13:34:25 +0900

    no thx2

--- no_thk/소스.cpp
+++ no_thk/소스.cpp
@@ -28,6 +28,7 @@ class Player {
 public:
     string name;
     int coins;
+    int score = 0;
     vector<int> cards;
 
     Player(string n) : name(n), coins(11) {}
@@ -38,6 +39,7 @@ public:
             cout << name << "는(은) 코인이 없어 카드 " << card << "를 받습니다." << endl;
           
             cards.push_back(card);
+            sort(cards.begin(), cards.end());
             coins += coinPool;
            // coinPool = 0;
             return true;
@@ -54,6 +56,7 @@ public:
         if (response == 'Y' || response == 'y') {
             cards.push_back(card);
             coins += coinPool;
+            sort(cards.begin(), cards.end());
           //  coinPool = 0;
             return true;
         }
@@ -65,18 +68,49 @@ public:
     }
 };
 
+
+void Score(Player& p)
+{
+    
+
+    if (p.cards.size() != 0)
+    {
+        int sub_score = p.cards[0];
+        int last = p.cards[0];
+        for (int a = 1; a < p.cards.size(); a++)
+        {
+            if (p.cards[a] == last + 1)
+            {
+                last = p.cards[a];
+                continue;
+            }
+            else
+            {
+                sub_score += p.cards[a];
+                last = p.cards[a];
+            }
+        }
+        p.score = -(sub_score);
+
+    }
+}
+
 void printPlayerStatus(const vector<Player>& players) {
     for (const auto& player : players) {
-        cout << player.name << " - 코인: " << player.coins << ", 카드: ";
+        cout << player.name << " - 코인: " << player.coins << "\n 카드: ";
         if (!player.cards.empty()) {
+
             for (const auto& card : player.cards) {
                 cout << card << " ";
             }
+
         }
-        else {
+        else 
             cout << "없음";
-        }
-        cout << endl;
+        
+        Score(const_cast<Player&>(player));
+        cout << "\n 현재 점수 : " << player.score<<"\n\n";
+       
     }
     cout << endl;
 }
@@ -110,7 +144,8 @@ int main() {
             printPlayerStatus(players);
             cardTaken = players[currentPlayerIndex].takeOrPassCard(deck[i], coinPool);
 
-            if (cardTaken) {
+            if (cardTaken) 
+            {
                 startingPlayerIndex = currentPlayerIndex;
                 cout << players[currentPlayerIndex].name << "가 카드 " << deck[i] << "를 받고 코인 풀에서 " << coinPool << " 코인을 받습니다." << endl;
                 coinPool = 0;
@@ -122,6 +157,28 @@ int main() {
         cout << "현재 코인 풀: " << coinPool << endl << endl;
         system("pause");
     }
+
+    cout << "\n게임이 끝났습니다. 최종 결과는 다음과 같습니다:\n";
+
+    // 각 플레이어의 최종 점수 계산
+    for (auto& player : players) {
+        player.score += player.coins; // 최종 점수는 점수 + 코인
+    }
+
+    // 점수를 기준으로 플레이어 정렬 (내림차순)
+    sort(players.begin(), players.end(), [](const Player& a, const Player& b) {
+        return a.score > b.score;
+        });
+
+    // 최종 점수와 등수 출력
+    int rank = 1;
+    for (const auto& player : players) {
+        cout << rank << "등: " << player.name << " - 점수: " << player.score << '\n';
+        rank++;
+    }
+
+    cout << endl;
+
 
     return 0;
 }
